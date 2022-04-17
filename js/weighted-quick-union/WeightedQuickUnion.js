7b4029788266c9589532bbd40191a1753be18e08
Parvin Farahzadeh <parv93farah@gmail.com>
2022-04-17 18:06:43 -0400

    add improvements to algorithm

---
+++ js/weighted-quick-union/WeightedQuickUnion.js
@@ -0,0 +1,33 @@
+export default class WeightedQuickUnion {
+    constructor (n) {
+        this.id = []; 
+        this.size = [];
+        for (var i = 0; i < n; i++) {
+            this.id[i] = i;
+            this.size[i] = 1;
+        }
+    }
+
+    root = (i) => {
+        while( i !== this.id[i]) i = this.id[i];
+        return i;
+    }
+
+    isConnected = (p,q) => {
+        return this.root(p) === this.root(q);
+    }
+
+    union = (p,q) => {
+        const pid = this.root(p); 
+        const qid = this.root(q);
+        if( pid === qid) return; 
+        if(this.size[pid] < this.size[qid]) {
+            this.id[pid] = qid;
+            this.size[qid] += this.size[pid];
+        }
+        else {
+            this.id[qid] = pid;
+            this.size[pid] += this.size[qid];
+        }
+    }
+}
