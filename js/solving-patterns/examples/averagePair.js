ddb611583242c0782c0edbc78fed46734ad99025
Parvin Farahzadeh <parv93farah@gmail.com>
2022-11-06 16:18:24 -0500

    average 2 pointers

---
+++ js/solving-patterns/examples/averagePair.js
@@ -0,0 +1,31 @@
+// Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+// Bonus Constraints:
+
+// Time: O(N)
+
+// Space: O(1)
+
+// Sample Input:
+
+
+function averagePair(args, average) {
+    args.sort((a, b) => { return (a - b)}); 
+
+    let leftIndex = 0; 
+    let i  = 1; 
+
+    while (i < args.length) {
+        if((args[i] + args[leftIndex]) / 2 === average) {
+            return true; 
+        }
+        leftIndex ++; 
+        i ++; 
+    }
+    return false;
+}
+
+console.log(averagePair([1,2,3],2.5)) // true
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) // true
+console.log(averagePair([-1,0,3,4,5,6], 4.1)) // false
+console.log(averagePair([],4)) // false
