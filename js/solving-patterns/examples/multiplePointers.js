ad19f3759f69388cdb1ec419e9518ab5b0fa4a5b
Parvin Farahzadeh <parv93farah@gmail.com>
2022-11-06 16:18:37 -0500

    duplicate multiple pointer

--- js/solving-patterns/examples/multiplePointers.js
+++ js/solving-patterns/examples/multiplePointers.js
@@ -9,28 +9,19 @@
 // Time - O(n log n)
 // Space - O(1)
 const areThereDuplicates = (...args) => {
-    if(args.length <= 0) return false; 
-
+    args.sort((a , b) => { return a - b}); 
     let leftIndex = 0;
-    
-    for (let i = 0; i < args.length; i++) {
-        
-        if (args[i] === args[leftIndex]) {
-            console.log(i, leftIndex);
-
-            if (leftIndex === i) {
-                leftIndex++;
-                continue;
-            }
-            return true; 
-        }
-        leftIndex++;
-
+    let i = 1;
+    while( i < args.length) {
+        if (args[leftIndex] == args[i]) return true;
+        leftIndex ++;
+        i++;
     }
-    return false; 
+    return false;
 }; 
 
 
-console.log(areThereDuplicates(1, 2, 3)); // false
+
+console.log(areThereDuplicates(1, 2, 3, 4, 5, 6, 7, 8)); // false
 console.log(areThereDuplicates(1, 2, 2)); // true 
 console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true 
