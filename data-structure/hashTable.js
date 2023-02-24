b8c3fc6d82897ac5186f614a894b5e146b8f8c8b
Parvin Farahzadeh <parv93farah@gmail.com>
2023-02-24 10:25:36 -0500

    hashtable class

---
+++ data-structure/hashTable.js
@@ -0,0 +1,70 @@
+class HashTable {
+  constructor() {
+    this.table = new Array(127); 
+    this.size = 0; 
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
+
+  get(key) {
+    const target = this._hash(key);
+    if (this.table[target]) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[target][i][0] === key) {
+          return this.table[target][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        // Find the key/value pair in the chain
+        if (this.table[index][i][0] === key) {
+          this.table[index][i][1] = value;
+          return;
+        }
+      }
+      // not found, push a new key/value pair
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [];
+      this.table[index].push([key, value]);
+    }
+    this.size++;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+  
+    if (this.table[index] && this.table[index].length) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index].splice(i, 1);
+          this.size--;
+          return true;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+
+  display() {
+    this.table.forEach((values, index) => {
+      const chainedValues = values.map(
+        ([key, value]) => `[ ${key}: ${value} ]`
+      );
+      console.log(`${index}: ${chainedValues}`);
+    });
+  }
+}
