e1fbbdcc06da1ce348a6df302910af2dcf03f34e
Parvin Farahzadeh <parv93farah@gmail.com>
2023-02-28 10:32:16 -0500

    remove duplicates

---
+++ problem-solving-questions/twoPointers/removeDuplicates.js
@@ -0,0 +1,22 @@
+var removeDuplicates = function(nums) {
+    if(nums.length === 0) return 0; 
+    let leftPointer = 0;
+    let uniqueNums = [nums[leftPointer]];
+    let count = 1; 
+
+    for(let i = 1; i < nums.length; i++) {
+      if (nums[leftPointer] !== nums[i]) {
+        uniqueNums.push(nums[i]);
+        leftPointer = i;
+        count ++;
+      }
+    }
+
+    for(let j = 0; j < nums.length - count; j++) {
+      uniqueNums.push(-121);
+    }
+
+    return count; 
+};
+
+removeDuplicates([11,,1,2,2,3,3,3,4,4,4,4])
