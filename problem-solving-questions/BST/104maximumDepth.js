d64e1d89ae63bdd9a8555a22b36f9130a92f4582
Parvin Farahzadeh <parv93farah@gmail.com>
2023-05-14 22:15:44 -0400

    max depth

---
+++ problem-solving-questions/BST/104maximumDepth.js
@@ -0,0 +1,8 @@
+var maxDepth = function(root) {
+  if(!root) return 0; 
+
+  let leftDepth = maxDepth(root.left); 
+  let rightDepth = maxDepth(root.right); 
+
+  return Math.max(leftDepth, rightDepth) + 1; 
+};
