693b0a0a0fb3ec80ac3d6798f45f6dd9fb9b686d
Parvin Farahzadeh <parv93farah@gmail.com>
2023-05-29 17:22:39 -0400

    word deletion trie

--- problem-solving-questions/BST/208implementTrie.js
+++ problem-solving-questions/BST/208implementTrie.js
@@ -42,6 +42,58 @@ Trie.prototype.startsWith = function(prefix) {
   return true
 };
 
+Trie.prototype.delete = function(word) {
+    let currentNode = this.root;
+    let lastBranchNode = null;
+    let lastBrachChar = 'a';
+  
+    for (let c of word) {
+        if (currentNode.childNode === null) {
+            return false;
+        } else {
+            let count = 0;
+            for (let i = 0; i < 26; i++) {
+                if (currentNode.childNode[i] !== null) {
+                    count++;
+                }
+            }
+  
+            if (count > 1) {
+                lastBranchNode = currentNode;
+                lastBrachChar = c;
+            }
+            currentNode = currentNode.childNode;
+        }
+    }
+  
+    let count = 0;
+    for (let i = 0; i < 26; i++) {
+        if (currentNode.childNode[i] !== null) {
+            count++;
+        }
+    }
+  
+    // Case 1: The deleted word is a prefix of other words
+    // in Trie.
+    if (count > 0) {
+        currentNode.wordCount--;
+        return true;
+    }
+  
+    // Case 2: The deleted word shares a common prefix with
+    // other words in Trie.
+    if (lastBranchNode !== null) {
+        lastBranchNode.childNode[lastBrachChar] = null;
+        return true;
+    }
+    // Case 3: The deleted word does not share any common
+    // prefix with other words in Trie.
+    else {
+        root.childNode[word[0].charCodeAt(0) - 'a'.charCodeAt(0)] = null;
+        return true;
+    }
+};
+
 /** 
 * Your Trie object will be instantiated and called as such:
 * var obj = new Trie()
